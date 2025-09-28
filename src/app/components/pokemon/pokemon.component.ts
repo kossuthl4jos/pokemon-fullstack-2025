df9e793243649c8032cbe64bdb41496419691912
Lajos Kossuth <kossuthl4jos@gmail.com>
2025-09-28 20:57:39 +0200

    adjust service to handle dynamic query params

--- src/app/components/pokemon/pokemon.component.ts
+++ src/app/components/pokemon/pokemon.component.ts
@@ -1,36 +1,51 @@
-import { TitleCasePipe } from "@angular/common";
-import { Component, inject } from "@angular/core";
-import { ActivatedRoute } from "@angular/router";
-import { injectQuery, QueryClient } from "@tanstack/angular-query-experimental";
-import { PokemonService } from "../../services/pokemon.service";
+import { TitleCasePipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { ActivatedRoute } from '@angular/router';
+import { QueryClient } from '@tanstack/angular-query-experimental';
+import { filter, map } from 'rxjs';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
-  selector: "app-pokemon",
-  standalone: true,
-  template: `
-    @if (query.data()) {
-    <h2>{{ pokemon.name | titlecase }}</h2>
-    <img [src]="pokemon.sprites.front_default" alt="{{ pokemon.name }}" />
-    <p><strong>Height:</strong> {{ pokemon.height }}</p>
-    <p><strong>Weight:</strong> {{ pokemon.weight }}</p>
-    <p><strong>Base Experience:</strong> {{ pokemon.base_experience }}</p>
-    }
-  `,
-  imports: [TitleCasePipe],
-  styleUrls: ["./pokemon.component.css"],
+    selector: 'app-pokemon',
+    standalone: true,
+    template: `
+        @if(pokemon){ @if (pokemon.isPending()) {
+        <p>Loading...</p>
+        } @else if (pokemon.isError()) {
+        <span>Error: {{ pokemon.error().message }}</span>
+        } @else { @if(pokemon.data(); as pokemon) {
+        <h2>{{ pokemon.name | titlecase }}</h2>
+        <img [src]="pokemon.sprites.other['official-artwork'].front_default" alt="{{ pokemon.name }}" />
+        <p>
+            <strong>Height:</strong>
+            {{ pokemon.height }}
+        </p>
+        <p>
+            <strong>Weight:</strong>
+            {{ pokemon.weight }}
+        </p>
+        <p>
+            <strong>Base Experience:</strong>
+            {{ pokemon.base_experience }}
+        </p>
+        } } }
+    `,
+    imports: [TitleCasePipe],
+    styleUrls: ['./pokemon.component.css'],
 })
 export class PokemonComponent {
-  queryClient = inject(QueryClient);
-  pokemonService = inject(PokemonService);
-  activatedRoute = inject(ActivatedRoute);
+    queryClient = inject(QueryClient);
+    pokemonService = inject(PokemonService);
+    activatedRoute = inject(ActivatedRoute);
 
-  pokemon: any;
+    pokemonId = toSignal(
+        this.activatedRoute.paramMap.pipe(
+            map(paramMap => paramMap.get('pokemonId')),
+            filter(Boolean)
+        ),
+        { initialValue: '' }
+    );
 
-  query = injectQuery(() => ({
-    queryKey: ["pokemons"],
-    queryFn: () =>
-      this.pokemonService.getPokemonById(
-        this.activatedRoute.snapshot.paramMap.get("pokemonId")!
-      ),
-  }));
+    pokemon = this.pokemonService.getPokemonById(this.pokemonId);
 }
--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -1,34 +1,29 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { lastValueFrom } from "rxjs";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, Signal } from '@angular/core';
+import { injectQuery } from '@tanstack/angular-query-experimental';
+import { lastValueFrom, map } from 'rxjs';
+import { Pokemon, PokemonsResponse } from './schemas';
 
 @Injectable({
-  providedIn: "root",
+    providedIn: 'root',
 })
 export class PokemonService {
-  private http = inject(HttpClient);
-  private baseUrl = "https://pokeapi.co/api/v2";
+    private http = inject(HttpClient);
+    private baseUrl = 'https://pokeapi.co/api/v2';
 
-  getPokemons(limit = 20, offset = 0): Promise<PokemonsResponse> {
-    return lastValueFrom(
-      this.http.get<PokemonsResponse>(`${this.baseUrl}/pokemon/`, {
-        params: { limit, offset },
-      })
-    );
-  }
+    getPokemons(limit = 20, offset = 0): Promise<PokemonsResponse> {
+        return lastValueFrom(
+            this.http
+                .get<PokemonsResponse>(`${this.baseUrl}/pokemon/`, {
+                    params: { limit, offset },
+                })
+                .pipe(map(response => PokemonsResponse.parse(response)))
+        );
+    }
 
-  getPokemonById(id: string): Promise<any> {
-    return lastValueFrom(this.http.get<any>(`${this.baseUrl}/pokemon/${id}`));
-  }
-}
-
-export interface Pokemon {
-  name: string;
-  url: string;
-}
-export interface PokemonsResponse {
-  count: number;
-  next: string;
-  previous: string;
-  results: Pokemon[];
+    getPokemonById = (pokemonId: Signal<string>) =>
+        injectQuery(() => ({
+            queryKey: ['pokemon', pokemonId()],
+            queryFn: () => lastValueFrom(this.http.get<Pokemon>(`${this.baseUrl}/pokemon/${pokemonId()}`).pipe(map(response => Pokemon.parse(response)))),
+        }));
 }
